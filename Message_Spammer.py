6dcda43d3ab5dcfab77cd83f590ef6f73d1de715
aftxrlifx <96382604+its-Jaxx@users.noreply.github.com>
2023-11-13 12:02:19 -0800

    Added the finished build of the code.

---
+++ Message_Spammer.py
@@ -0,0 +1,31 @@
+import random
+import time
+import pyautogui as pui
+
+messages = [
+    'test1',
+    'test2',
+    'test3'
+]
+
+def numbers_only(s):
+    return int(''.join(filter(str.isdigit, s)))
+
+q = input("Enter amount of messages to send: ")
+x = numbers_only(q)
+
+print(f"\nSending {x} messages in:")
+for i in range(5, 0, -1):
+    print(i)
+    time.sleep(1)
+
+for _ in range(x):
+    a = random.choice(messages)
+    pui.write(f"Hello {a}!")
+    pui.press('enter')
+
+if x == 1:
+    print("\n1 Message Sent!")
+
+elif x > 1:
+    print(f"\n{x} Messages Sent!")
